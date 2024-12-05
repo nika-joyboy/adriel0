ad6ff79138b471bcd570bd4b6d6ae1ab52b22374
nika-joyboy <00001108115251sp@al.educacao.sp.gov.br>
2024-12-05 16:34:18 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'luta',
+    'Qual é a técnica mais eficaz?',
+    'Depende do contexto, mas o jiu-jitsu é eficaz em combate corpo a corpo.'
+)
+
+criaCartao(
+    'luta',
+    'Como melhorar a defesa?',
+    'Treine reflexos e postura, mantenha a guarda alta e mova-se bem.'
+)
+
+criaCartao(
+    'luta',
+    'Quais golpes são básicos?',
+    'Jab, cruzado, chute frontal e queda básica.'
+)
+
+criaCartao(
+    'luta',
+    'Qual o melhor estilo para iniciantes?',
+    'Boxe ou jiu-jitsu, pela simplicidade e aplicabilidade '
+)
